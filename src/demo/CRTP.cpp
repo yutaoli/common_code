b73176337c9a1fd46547d2857a9d10b0808aeedb
yutaoli <1213617849@qq.com>
2023-05-10 12:49:05 +0800

    CRTP

---
+++ src/demo/CRTP.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// 基类模板
+template <typename Derived>
+class Base {
+public:
+    void foo() {
+        static_cast<Derived*>(this)->fooImpl();
+    }
+};
+
+// 派生类
+class Derived : public Base<Derived> {
+public:
+    void fooImpl() {
+        printf("%s\n",__FUNCTION__);
+        // 实现派生类特定的行为
+        // ...
+    }
+};
+
+int main() {
+    Derived d;
+    d.foo();  // 调用派生类的 fooImpl 函数
+    return 0;
+}
