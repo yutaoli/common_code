ba81a2e1b617b36be72fc891797b254a87ff8c41
yutaoli <1213617849@qq.com>
2023-05-31 12:55:00 +0800

    computing_types

---
+++ src/demo/test_template/computing_types/sqrt2.hpp
@@ -0,0 +1,38 @@
+/* The following code example is taken from the book
+ * "C++ Templates - The Complete Guide"
+ * by David Vandevoorde and Nicolai M. Josuttis, Addison-Wesley, 2002
+ *
+ * (C) Copyright David Vandevoorde and Nicolai M. Josuttis 2002.
+ * Permission to copy, use, modify, sell and distribute this software
+ * is granted provided this copyright notice appears in all copies.
+ * This software is provided "as is" without express or implied
+ * warranty, and with no claim as to its suitability for any purpose.
+ */
+#include "ifthenelse.hpp"
+
+// primary template for main recursive step
+template<int N, int LO=1, int HI=N>
+class Sqrt {
+  public:
+    // compute the midpoint, rounded up
+    enum { mid = (LO+HI+1)/2 };
+
+    // search a not too large value in a halved interval
+    typedef typename IfThenElse<(N<mid*mid),
+                                Sqrt<N,LO,mid-1>,
+                                Sqrt<N,mid,HI> >::ResultT
+            SubT;
+    enum { result = SubT::result };
+};
+
+// partial specialization for end of recursion criterion
+template<int N, int S>
+class Sqrt<N, S, S> {
+  public:
+    enum { result = S };
+};
+
+/*
+ 用 IfThenElse 的目的是为了减少Sqrt的个数，如果用if，则编译器会同时展开Sqrt<N,LO,mid-1>和Sqrt<N,mid,HI> ，
+ 用 IfThenElse 则只会展开一个，如果是true，则展开Sqrt<N,LO,mid-1>，如果是false，则展开Sqrt<N,mid,HI>
+ */
